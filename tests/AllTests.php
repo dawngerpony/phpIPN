69916fb7125c71784fb78a48421e7b2d8941604e
Dafydd James <dafydd@cantab.net>
2011-09-18 12:40:49 +0100

    Fixed to (hopefully) be runnable from any directory.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -2,7 +2,10 @@
 if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'AllTests::main');
 }
- 
+
+$path = dirname(__file__);
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
 require_once 'PHPUnit/TextUI/TestRunner.php';
  
 require_once 'DBManagerTest.php';
